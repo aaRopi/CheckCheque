7038cba9315526eb74328b2f9ad9f8a1e9632519
pRopia <pratik.ropia@gmail.com>
2019-08-05 16:49:41 +0200

    Removed ReactiveUI and associated stuff.

--- Application/CheckCheque/CheckCheque/AppBootStrapper.cs
+++ Application/CheckCheque/CheckCheque/AppBootStrapper.cs
@@ -1,19 +1,19 @@
-﻿using CheckCheque.ViewModels;
-using CheckCheque.Views;
-using ReactiveUI;
-using Splat;
+﻿//using CheckCheque.ViewModels;
+//using CheckCheque.Views;
+//using ReactiveUI;
+//using Splat;
 
-namespace CheckCheque
-{
-    public class AppBootStrapper : ReactiveObject
-    {
-        public RoutingState Router { get; protected set; }
+//namespace CheckCheque
+//{
+//    public class AppBootStrapper : ReactiveObject
+//    {
+//        public RoutingState Router { get; protected set; }
 
-        public AppBootStrapper()
-        {
-            Router = new RoutingState();
-            Locator.CurrentMutable.Register(() => new AddInvoicesPage(), typeof(IViewFor<AddInvoicesPageViewModel>));
-            Locator.CurrentMutable.Register(() => new InvoicesPage(), typeof(IViewFor<InvoicesViewModel>));
-        }
-    }
-}
+//        public AppBootStrapper()
+//        {
+//            Router = new RoutingState();
+//            Locator.CurrentMutable.Register(() => new AddInvoicesPage(), typeof(IViewFor<AddInvoicesPageViewModel>));
+//            Locator.CurrentMutable.Register(() => new InvoicesPage(), typeof(IViewFor<InvoicesViewModel>));
+//        }
+//    }
+//}
